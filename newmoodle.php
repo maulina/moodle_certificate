a53671e8eb6213f095769028b5d3595b5894132d
Maulina Andhini <maulina.andhini@gmail.com>
2014-04-30 20:17:57 +0700

    Create newmoodle.php

---
+++ newmoodle.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'newmodule';
+$string['modulenameplural'] = 'newmodules';
+$string['modulename_help'] = 'Use the newmodule module for... | The newmodule module allows...';
+$string['newmodulefieldset'] = 'Custom example fieldset';
+$string['newmodulename'] = 'newmodule name';
+$string['newmodulename_help'] = 'This is the content of the help tooltip associated with the newmodulename field. Markdown syntax is supported.';
+$string['newmodule'] = 'newmodule';
+$string['pluginadministration'] = 'newmodule administration';
+$string['pluginname'] = 'newmodule';
