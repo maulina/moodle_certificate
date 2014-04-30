ee4caa23c2c3046e2f9a535e5ac38a10c6b3636c
Maulina Andhini <maulina.andhini@gmail.com>
2014-04-30 20:27:40 +0700

    Create access.php

---
+++ access.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+
+/***************************** remove these comment marks and modify the code as needed
+    'mod/newmodule:view' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'legacy' => array(
+            'guest' => CAP_ALLOW,
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'admin' => CAP_ALLOW
+        )
+    ),
+
+    'mod/newmodule:submit' => array(
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'legacy' => array(
+            'student' => CAP_ALLOW
+        )
+    ),
+******************************/
+);
+
