9dba466739c808ba165e60ff4c54addf0e8b0c3a
Maulina Andhini <maulina.andhini@gmail.com>
2014-04-30 20:36:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,93 @@
 <?php
 
-require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-require_once(dirname(__FILE__).'/lib.php');
 
-$id = required_param('id', PARAM_INT);   // course
+require_once('../../config.php');
+require_once('lib.php');
 
-$course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
+$id = required_param('id', PARAM_INT);           // Course Module ID
 
+// Ensure that the course specified is valid
+if (!$course = $DB->get_record('course', array('id'=> $id))) {
+    print_error('Course ID is incorrect');
+}
+
+// Requires a login
 require_course_login($course);
 
-add_to_log($course->id, 'newmodule', 'view all', 'index.php?id='.$course->id, '');
+// Declare variables
+$currentsection = "";
+$printsection = "";
+$timenow = time();
 
-$coursecontext = get_context_instance(CONTEXT_COURSE, $course->id);
+// Strings used multiple times
+$strcertificates = get_string('modulenameplural', 'certificate');
+$strissued  = get_string('issued', 'certificate');
+$strname  = get_string("name");
+$strsectionname = get_string('sectionname', 'format_'.$course->format);
 
-$PAGE->set_url('/mod/newmodule/index.php', array('id' => $id));
-$PAGE->set_title(format_string($course->fullname));
-$PAGE->set_heading(format_string($course->fullname));
-$PAGE->set_context($coursecontext);
+// Print the header
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_url('/mod/certificate/index.php', array('id'=>$course->id));
+$PAGE->navbar->add($strcertificates);
+$PAGE->set_title($strcertificates);
+$PAGE->set_heading($course->fullname);
+
+// Add the page view to the Moodle log
+add_to_log($course->id, 'certificate', 'view all', 'index.php?id='.$course->id, '');
+
+// Get the certificates, if there are none display a notice
+if (!$certificates = get_all_instances_in_course('certificate', $course)) {
+    echo $OUTPUT->header();
+    notice(get_string('nocertificates', 'certificate'), "$CFG->wwwroot/course/view.php?id=$course->id");
+    echo $OUTPUT->footer();
+    exit();
+}
+
+if ($usesections = course_format_uses_sections($course->format)) {
+    $sections = get_all_sections($course->id);
+}
+
+$table = new html_table();
+
+if ($usesections) {
+    $table->head  = array ($strsectionname, $strname, $strissued);
+} else {
+    $table->head  = array ($strname, $strissued);
+}
+
+foreach ($certificates as $certificate) {
+    if (!$certificate->visible) {
+        // Show dimmed if the mod is hidden
+        $link = html_writer::tag('a', $certificate->name, array('class' => 'dimmed',
+            'href' => $CFG->wwwroot . '/mod/certificate/view.php?id=' . $certificate->coursemodule));
+    } else {
+        // Show normal if the mod is visible
+        $link = html_writer::tag('a', $certificate->name, array('class' => 'dimmed',
+            'href' => $CFG->wwwroot . '/mod/certificate/view.php?id=' . $certificate->coursemodule));
+    }
+    if ($certificate->section !== $currentsection) {
+        if ($certificate->section) {
+            $printsection = $certificate->section;
+        }
+        if ($currentsection !== "") {
+            $table->data[] = 'hr';
+        }
+        $currentsection = $certificate->section;
+    }
+    // Get the latest certificate issue
+    if ($certrecord = $DB->get_record('certificate_issues', array('userid' => $USER->id, 'certificateid' => $certificate->id))) {
+        $issued = userdate($certrecord->timecreated);
+    } else {
+        $issued = get_string('notreceived', 'certificate');
+    }
+    if (($course->format == 'weeks') || ($course->format == 'topics')) {
+        $table->data[] = array ($certificate->section, $link, $issued);
+    } else {
+        $table->data[] = array ($link, $issued);
+    }
+}
 
 echo $OUTPUT->header();
-
-if (! $newmodules = get_all_instances_in_course('newmodule', $course)) {
-    notice(get_string('nonewmodules', 'newmodule'), new moodle_url('/course/view.php', array('id' => $course->id)));
-}
-
-if ($course->format == 'weeks') {
-    $table->head  = array(get_string('week'), get_string('name'));
-    $table->align = array('center', 'left');
-} else if ($course->format == 'topics') {
-    $table->head  = array(get_string('topic'), get_string('name'));
-    $table->align = array('center', 'left', 'left', 'left');
-} else {
-    $table->head  = array(get_string('name'));
-    $table->align = array('left', 'left', 'left');
-}
-
-foreach ($newmodules as $newmodule) {
-    if (!$newmodule->visible) {
-        $link = html_writer::link(
-            new moodle_url('/mod/newmodule.php', array('id' => $newmodule->coursemodule)),
-            format_string($newmodule->name, true),
-            array('class' => 'dimmed'));
-    } else {
-        $link = html_writer::link(
-            new moodle_url('/mod/newmodule.php', array('id' => $newmodule->coursemodule)),
-            format_string($newmodule->name, true));
-    }
-
-    if ($course->format == 'weeks' or $course->format == 'topics') {
-        $table->data[] = array($newmodule->section, $link);
-    } else {
-        $table->data[] = array($link);
-    }
-}
-
-echo $OUTPUT->heading(get_string('modulenameplural', 'newmodule'), 2);
+echo '<br />';
 echo html_writer::table($table);
 echo $OUTPUT->footer();
